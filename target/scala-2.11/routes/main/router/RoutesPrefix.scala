1fa546c110a5a1dfe33d44d6d5217d0f1e491784
kroffo <kroffo@oswego.edu>
2017-03-22 20:15:58 -0400

    Initialized play project.

---
+++ target/scala-2.11/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,16 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/kennethroffo/Documents/PlayStockMarketSimulator/conf/routes
+// @DATE:Wed Mar 22 19:13:07 EDT 2017
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
