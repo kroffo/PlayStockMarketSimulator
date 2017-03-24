675bc29ac556f3dea39f29788332eafce45bb9a8
Stephen DiCerce <sdicerce@oswego.edu>
2017-03-23 22:10:00 -0400

    Updated UserQuery and Users: DEBUGGING COMMIT ONLY

--- app/controllers/UserQuery.java
+++ app/controllers/UserQuery.java
@@ -13,61 +13,63 @@ import java.io.InputStream;
 import java.io.IOException;
 
 public class UserQuery extends Controller {
-	
+    
+    //GET
     public Result getUser(String name) {
 	
 	services.User u = services.User.getUser(name);	    
 	if(u == null) {
-	    return badRequest("Not Found"); // CHANGE THIS TO 500 ERROR
+	    return status(500);
 	}
 	
 	String json = getJsonForUser(u);
 	return ok(json);
     }
     
-//     @PUT
-// 	@Produces(MediaType.APPLICATION_JSON)
-// 	@Consumes(MediaType.APPLICATION_JSON)
-// 	public Response updateUser (@PathParam("name") String name, InputStream data) {
-// 	services.User u = services.User.loadUser(name);
-// 	if (u == null) { 
-// 	    return Response.status(Response.Status.NOT_FOUND).build();
-// 	}
+    //PUT
+    public Result updateUser (String name) {
+	JsonNode json = request().body().asJson();
+	if (json == null)
+	    return badRequest("Expecting Json data");
+
+	services.User u = services.User.loadUser(name);
+	if (u == null) { 
+	    return status(404, "User with the name " + name + "does not exist."); 
+	}
 	
-// 	String json = "";
-// 	try {
-// 	    BufferedReader in = new BufferedReader(new InputStreamReader(data));
-// 	    String line = null;
-// 	    while ((line = in.readLine()) != null)
-// 		json += line;
+	String json = "";
+	try {
+	    BufferedReader in = new BufferedReader(new InputStreamReader(data));
+	    String line = null;
 	    
-// 	    JSONObject obj = new JSONObject(json);
-// 	    String password = obj.getString("password");
+	    String password = json.findPath("password").textValue();
+
+	    if(password == null)
+		return badRequest("Missing parameter [password]");
 	    
-// 	    if (!services.User.updateUser(name, password)){
-// 		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-// 	    }
+	    if (!services.User.updateUser(name, password)){
+		return status(500);
+	    }
 	    
-// 	} catch(IOException | JSONException e) {
-// 	    return Response.status(Response.Status.BAD_REQUEST).build();
-// 	}
-// 	return Response.status(Response.Status.NO_CONTENT).build();
-//     }
+	} catch(IOException | JSONException e) {
+	    return status(400);
+	}
+	return status(204);
+    }
     
-//     @DELETE
-// 	@Produces(MediaType.APPLICATION_JSON)
-// 	public Response deleteUser( @PathParam("name") String name ) {
-// 	services.User u = services.User.loadUser(name);
-// 	if(u == null) {
-// 	    return Response.status(Response.Status.NOT_FOUND).build();
-// 	}
+    //DELETE
+    public Result deleteUser( String name ) {
+	services.User u = services.User.loadUser(name);
+	if(u == null) {
+	    return status(404);
+	}
 	
-// 	if(!services.User.deleteUser(name)) {
-// 	    return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-// 	}
-// 	String json = getJsonForUser(u);
-// 	return Response.ok(json, MediaType.APPLICATION_JSON).build();
-//     }
+	if(!services.User.deleteUser(name)) {
+	    return status(500);
+	}
+	String json = getJsonForUser(u);
+	return ok(json);
+    }
      
     private String getJsonForUser(services.User u) {
 	String json = "{\n";
--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -14,7 +14,8 @@ import java.io.InputStream;
 import java.io.IOException;
 
 public class Users extends Controller {
-    
+
+    //GET
     public Result getUsers(String sortingMethod, String companyName) {
 	services.User[] users = services.User.getUsers();
 	services.Company[] companies = services.Company.getCompanies();
@@ -141,141 +142,34 @@ public class Users extends Controller {
 	    }
 	    json += "]";
 	}
-	// 	    if ((sortingMethod.equals("money") || sortingMethod.equals("total_money") || 
-	// 		 sortingMethod == null || sortingMethod.equals("")) && 
-	// 		(companyName == null || companyName.equals(""))) {
-	// 		if (sortingMethod.equals("money")){
-	// 		    for (int i = 0; i < users.length; ++i) {
-	// 			for (int j = i + 1; j < users.length; ++j) {
-	// 			    if (users[j].getMoney() > users[i].getMoney()) {
-	// 				temp = users[i];
-	// 				users[i] = users[j];
-	// 				users[j] = temp;
-	// 			    }
-	// 			}
-	// 		    }
-	
-	// 		} else if (sortingMethod.equals("total_money")) {
-	// 		    for (int i = 0; i < users.length; ++i) {
-	// 			double totalMoneyI = users[i].getMoney() + users[i].getStockValue();
-	// 			for (int j = i + 1; j < users.length; ++j) {
-	// 			    double totalMoneyJ = users[j].getMoney() + users[j].getStockValue();
-	// 			    if (totalMoneyJ > totalMoneyI) {
-	// 				temp = users[i];
-	// 				users[i] = users[j];
-	// 				users[j] = temp;
-	// 			    }
-	// 			}
-	// 		    }
-	
-	// 		}
-	// 		json = "[\n";
-	// 		for(int i = 0, length = users.length; i < length; ++i) {
-	// 		    services.User u = users[i];
-	// 		    UriBuilder builder = uriInfo.getAbsolutePathBuilder();
-	// 		    String currentPath = builder.build().toString();
-	// 		    String uname = u.getName();
-	// 		    double totalMoney = u.getMoney() + u.getStockValue();
-	// 		    json += "  {\n";
-	// 		    json += "    \"name\": \"" + uname + "\",\n";
-	// 		    json += "    \"password\": \"" + u.getPassword() + "\",\n";
-	// 		    json += "    \"money\": " + u.getMoney() + ",\n";
-	// 		    json += "    \"stockValue\": " + u.getStockValue() + ",\n";
-	// 		    json += "    \"totalMoney\": " + totalMoney + ",\n";
-	// 		    json += "    \"links\": [\n";
-	// 		    json += "      {\n";
-	// 		    json += "        \"rel\": \"self\",\n";
-	// 		    json += "        \"href\": \"" + currentPath + "/" + uname + "\"\n";;
-	// 		    json += "      }\n";
-	// 		    json += "    ]\n";
-	// 		    json += "  }";
-	// 		    if(i < length - 1)
-	// 			json += ",";
-	// 		    json += "\n";
-	// 		}
-	// 		json += "]";
-	// 	    } else if(sortingMethod.equals("stocks")) {
-	// 		for (int i = 0; i <users.length; ++i) {
-	// 		    for (int j = i + 1; j <users.length; ++j) {
-	// 			if (users[j].getNumberOfStocks(companyName) > users[i].getNumberOfStocks(companyName)) {
-	// 			    temp = users[i];
-	// 			    users[i] = users[j];
-	// 			    users[j] = temp;
-	// 			}
-	// 		    }
-	// 		}
-	
-	// 		json = "[\n";
-	// 		json += "{";
-	// 		json += "\"users\" : [";
-	// 		for (int i = 0; i < users.length; ++i) {
-	// 		    services.User u = users[i];
-	// 		    services.Company company = services.Company.getCompany(companyName);
-	// 		    UriBuilder builder = uriInfo.getAbsolutePathBuilder();
-	// 		    String currentPath = builder.build().toString();
-	// 		    String uname = u.getName();
-	// 		    int row = i+1;
-	// 		    double total;
-	// 		    json += "{";
-	// 		    json += "\"name\" : \"" + u.getName() + "\",";
-	// 		    int stocks = u.getNumberOfStocks(companyName);
-	// 		    json += "\"numberStocks\" : " + stocks + ",";
-	// 		    double averagePrice = u.getAveragePriceBoughtAt(companyName);
-	// 		    json += "\"averagePrice\" : " + String.format("%.2f", averagePrice) + ",";
-	// 		    total = stocks * averagePrice;
-	// 		    json += "\"totalInvestment\" : "+ String.format("%.2f", total);
-	// 		    json +="}";
-	// 		    json += "    \"links\": [\n";
-	//                     json += "      {\n";
-	//                     json += "        \"rel\": \"self\",\n";
-	//                     json += "        \"href\": \"" + currentPath + "/" + uname + "\"\n";;
-	//                     json += "      }\n";
-	//                     json += "    ]\n";
-	// 		    if (i != users.length - 1)
-	// 			json += ",";
-	// 		}
-	// 		json += "]\n";
-	// 		json += "}\n";
-	// 		json += "]";
-	// 	    }
-	
+
 	return ok(json);
     }
     
-    // 	@POST
-    // 	    @Consumes(MediaType.APPLICATION_JSON)
-    // 	    @Produces(MediaType.APPLICATION_JSON)
-    // 	    public Response postUsers (InputStream data) {
-    
-    // 	    String json = "";
-    // 		try {
-    // 		    BufferedReader in = new BufferedReader(new InputStreamReader(data));
-    // 		    String line = null;
-    // 		    while((line = in.readLine()) != null)
-    // 			json += line;
-    
-    // 		    JSONObject obj = new JSONObject(json);
-    // 		    String name = obj.getString("name");
-    // 		    String password = obj.getString("password");
-    
-    // 		    if( name == null || password == null || password.equals("")
-    // 			|| name.equals("") )
-    // 			return Response.status(Response.Status.BAD_REQUEST).entity("Must provide a name.").build();
-    
-    // 		    services.User u = services.User.loadUser(name);
-    // 		    if(u != null) {
-    // 			return Response.status(Response.Status.CONFLICT).entity("User " + name + " already exists").build();
-    // 		    }
-    
-    // 		    if(!services.User.addUser(name, password)) {
-    // 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-    // 		    }
-    
-    // 		} catch(IOException | JSONException e) {
-    // 		    return Response.status(Response.Status.BAD_REQUEST).build();
-    // 		}
-    // 		return Response.status(Response.Status.CREATED).build();
-    // 	}
+    //POST
+    @BodyParser.Of(Json.class)
+	public Result postUsers () {
+	JsonNode json = request().body().asJson();
+	if(json == null)
+	    return badRequest("Expecting Json data");
+	
+	String name = json.findPath("name").textValue();
+	String password = json.findPath("password").textValue();
+	if( name == null || password == null || password.equals("")
+	    || name.equals("") )
+	    return status(400);
+	
+	services.User u = services.User.loadUser(name);
+	if(u != null) {
+	    return status(409);
+		}
+	
+	if(!services.User.addUser(name, password)) {
+	    return status(500);
+	}
+	
+	return status(201);
+    }
     
     
 }
